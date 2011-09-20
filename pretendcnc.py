e733edb277024f8c6629877bc93a6573ca96f898
timkrins <timkrins@gmail.com>
2011-09-20 21:48:46 +1000

    Works correctly now. Does not check the values it is given, other than the basics

--- pretendcnc.py
+++ pretendcnc.py
@@ -1,12 +1,8 @@
 import serial, sys, time
-try:
-    from colorama import Fore, Style, init
-    colorama = True
-except:
-    colorama = False
-    sys.stdout.write("[no colorama module]\n")
+from colorama import Fore, Style, init
 
 def DualWrite(*args):
+    time.sleep(0.05)
     ser.write(args[0].encode('utf-8'))
     first_printable1 = args[0].replace("\r", ":")
     first_printable2 = first_printable1.replace("\n", ":")
@@ -20,21 +16,13 @@ def DualWrite(*args):
         color = args[2]
         pass
     except:
-        try:
-            color = Fore.BLUE
-        except:
-            pass
+        color = Fore.BLUE
         pass
-    if(colorama):
-        sys.stdout.write(color+"  "+second+Fore.RED+"  "+first_printable2+"\n")
-    else:
-        sys.stdout.write("  "+second+"  "+first_printable2+"\n")
+    sys.stdout.write(color+"  "+second+Fore.RED+"  "+first_printable2+"\n")
     return ""
 
 def YellowTerm(text):
-    if(colorama):
-        sys.stdout.write(Style.BRIGHT+Fore.YELLOW)
-    sys.stdout.write("\t"+text)
+    sys.stdout.write(Style.BRIGHT+Fore.YELLOW+"\t"+text)
     return ""
 
 def SerialSetup():
@@ -51,38 +39,42 @@ def DumpSettings():
     DualWrite("$6 =  0.1 (mm/arc segment)\r\n")
     DualWrite("$7 =  11 (step port invert mask. binary = 3)\r\n")
     DualWrite("$8 =  1 (acceleration in mm/sec^2)\r\n")
-    DualWrite("$9 =  50.0 (max instant cornering speed change in delta mm/min)\r\n")
-    DualWrite("\r\n\'$x=value\' to set parameter or just \'$\' to dump current settings\r\n")
+    DualWrite("$9 =  50.0 (max instant cornering speed change in delta mm/min)\r\n\r\n")
+    DualWrite("'$x=value\' to set parameter or just \'$\' to dump current settings\r\n")
     return ""
     
 def processline(line):
     if('G01' in line):
-        if(colorama):
-            DualWrite("ok\n\r","Sent OK signal, G01 was detected",Fore.BLUE)
-        else:
-            DualWrite("ok\n\r","Sent OK signal, G01 was detected")
+        DualWrite("ok\n\r","Sent OK signal, G01 was detected",Fore.BLUE)
+    elif('G00' in line):
+        DualWrite("ok\n\r","Sent OK signal, G00 was detected",Fore.BLUE)
+    elif('X' in line):
+        DualWrite("ok\n\r","Sent OK signal, X was detected",Fore.BLUE)
+    elif('Y' in line):
+        DualWrite("ok\n\r","Sent OK signal, Y was detected",Fore.BLUE)
+    elif('Z' in line):
+        DualWrite("ok\n\r","Sent OK signal, Z was detected",Fore.BLUE)
+    elif(line == "$"):
+        DumpSettings()
     else:
-        if(colorama):
-            DualWrite("ok,"+line+"\n\r","Sent OK signal",Fore.YELLOW)
-        else:
-            DualWrite("ok,"+line+"\n\r","Sent OK signal")
+        DualWrite("Could not determine command, \""+line+"\"\n\r","ERROR ->", Fore.YELLOW)
+    return ""
                     
 def processchunk():
     popped = []
     while ("\n" in currentline) or ("\r" in currentline):
-            #currentlinestr = "".join(popped)
+        #currentlinestr = "".join(popped)
         currentpop = currentline.pop(0)
         popped.extend(currentpop)
     else:
         if(popped):
+            popped.pop()
             processline("".join(popped))
             popped = []
-        
-
-if(colorama):
-    init()
+    return ""
+   
+init()
 ser = SerialSetup()
-
 
 YellowTerm("pretendcnc.py, hackmelbourne CNC simulator.\n\n")
 #raw_input("Press Enter to start simulation.")
@@ -90,9 +82,8 @@ DualWrite("\n\rGrbl 0.6b\n\r","Sent Grbl version.")
 time.sleep(0.5)
 DumpSettings()
 currentline = []
-while True:  
+while True:
     processchunk()
-    #time.sleep(0.5)
     chunked = ser.readline()
     if(chunked):
-        currentline.extend(list(chunked.decode('utf-8')))
+        currentline.extend(list(chunked.decode('utf-8').upper()))
